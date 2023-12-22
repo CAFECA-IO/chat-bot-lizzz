233bcff6ddeff60bd981edaf96d34b2ef4330eaa
Liz <mingreal75@gmail.com>
2023-12-22 17:03:05 +0800

    test/ create page_index & component_message

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Message } from '@/components/message';
+
+const Homepage = () => {
+  return (
+    <main>
+      Container
+      <div>
+        Message Box
+        <div>
+          <Message />
+        </div>
+      </div>
+      <div>Input Box</div>
+    </main>
+  );
+};
+
+export default Homepage;
