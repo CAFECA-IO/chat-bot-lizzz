9ae0d190a8682e17b46210f8c387525b06a3171e
Liz <mingreal75@gmail.com>
2023-12-25 15:08:43 +0800

    preCommit steps(Git Hook & Test)

---
+++ test/jest.config.js
@@ -0,0 +1,5 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+export const coverageDirectory = 'coverage';
+export const preset = 'ts-jest';
+export const testEnvironment = 'node';
+export const testRegex = '(/tests/.*|(\\.|/)(test|spec))\\.tsx?$';
