6eecba154797d1d730f300519a13daa8df7ce494
Liz <mingreal75@gmail.com>
2023-12-25 18:57:01 +0800

    12/25 tailwind css install

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import { Message } from '@/components/message';
 
 const Homepage = () => {
   return (
     <main>
       Container
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
       <div>
         Message Box
         <div>
@@ -12,6 +12,8 @@ const Homepage = () => {
         </div>
       </div>
       <div>Input Box</div>
+      <div className='text-white bg-blue-500 p-4'>This is a blue box with white text.</div>
+      <div className='text-blue-500 bg-gray-200 p-4'>This is a box with blue text on a gray background.</div>
     </main>
   );
 };
