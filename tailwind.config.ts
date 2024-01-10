fdd20a0e2060b21fe7c1fd35bd14b97fae197387
Ming (●'◡'●) <mingreal75@gmail.com>
2024-01-10 11:01:02 +0800

    change tailwindcss file type to .ts

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [],
+  theme: {
+    extend: {
+      minHeight: {
+        128: '32rem',
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config
+
