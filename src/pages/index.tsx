30ba7d46947b511ed5bb59b93f7e50f6d34888fc
Liz <mingreal75@gmail.com>
2024-01-10 15:15:00 +0800

    add paper plane icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -105,10 +105,10 @@ const Homepage = () => {
 
         {/* Input Box */}
         <div className="px-6 pb-8 ">
-          <div className="flex items-center justify-between gap-6 rounded-3xl border border-gray-400 px-3 py-4 ">
-            <div className="flex-1">
+          <div className="flex items-center justify-between gap-6 rounded-3xl border border-gray-400 px-4 py-1">
+            <div className="flex-auto	">
               <input
-                className="size-full outline-none focus:outline-none"
+                className="w-full leading-loose outline-none focus:outline-none"
                 type="text"
                 value={userInput}
                 placeholder="Say something..."
@@ -116,11 +116,14 @@ const Homepage = () => {
                 onKeyUp={handleKeyUp}
               />
             </div>
-            <div className=" text-center	">
-              <Button className="text-amber-500" onClick={handleSendMessage}>
-                Send
-              </Button>
-            </div>
+            <Button className="p-2" onClick={handleSendMessage}>
+              <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512">
+                <path
+                  fill="#ff8f0f"
+                  d="M16.1 260.2c-22.6 12.9-20.5 47.3 3.6 57.3L160 376V479.3c0 18.1 14.6 32.7 32.7 32.7c9.7 0 18.9-4.3 25.1-11.8l62-74.3 123.9 51.6c18.9 7.9 40.8-4.5 43.9-24.7l64-416c1.9-12.1-3.4-24.3-13.5-31.2s-23.3-7.5-34-1.4l-448 256zm52.1 25.5L409.7 90.6 190.1 336l1.2 1L68.2 285.7zM403.3 425.4L236.7 355.9 450.8 116.6 403.3 425.4z"
+                />
+              </svg>
+            </Button>
           </div>
         </div>
       </div>
